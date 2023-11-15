e8f005e0debf840faa0358caa5f49ecc8b1e25ae
Santiago <guilhermesantiagosilva1203@gmail.com>
2023-11-15 16:57:34 -0300

    Criando estruturas de decissões

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
-let name = ""
-let xp = 0
+let nickname = ""
+let xp = 1.000
+
+if (xp <= 1.000){
+    console.log(nickname + " está no Nível FERRO" )
+}
+else if ((xp > 1.000) && (xp < 2.001)){
+    console.log( " está no nível BRONZE")
+}
+else if((xp > 2.000) && (xp < 5.001)){
+    console.log( " está no nível PRATA")
+}
+else if((xp > 5.000) && (xp < 7.001)){
+    console.log( " está no Nível OURO")
+}
+else if((xp > 7.000) && (xp < 8.001 )){
+    console.log (" está no Nível PLATINA")
+}
+else if((xp > 8.000) && (xp < 9.001 )){
+    console.log (" está no Nível ASCENDENTE")
+}
+else if((xp > 9.000) && (xp < 10.001 )){
+    console.log (" está no Nível Imortal")
+}
+else{
+
+}
 
